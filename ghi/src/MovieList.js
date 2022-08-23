1c8f5d88f221609965446ae3efe7629a2e62a61e
Xiaoteng Xia <xiaxiaott@gmail.com>
2022-08-23 15:44:16 -0500

    list page test ok

---
+++ ghi/src/MovieList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+class MovieList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: []
+        }
+    }
+
+    
+    async componentDidMount() {
+        let movie_id;
+        // const MTDB_KET = "742276fc88b89a452ad9c04ac04df00e"
+        const result = []
+        for (let i=2; i<5; i++){
+            movie_id = i;
+            const movie_url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`
+            console.log(movie_url)
+            const response = await fetch(movie_url);
+            if (response.ok && response.status === 200) {
+                const data = await response.json();
+                // data.movie_detail_url = `movie/${data.id}/`
+                result.push(data)
+                console.log(movie_id)
+                console.log(data)
+            };
+            this.setState({movies: result});
+        }
+    }
+
+
+    render() {
+        return(
+        <div>
+            {this.state.movies.map(movie => {
+                return (
+                    <Link to={"movie/" + movie.id + "/"} key={movie.id} className="btn btn-primary btn-lg px-4 gap-3">
+                        <div>
+                            <p>##################</p>
+                            <p>movie_name: { movie.title }</p>
+                            <p>movie_rate: { movie.vote_average }</p>
+                            <p>movie_id: { movie.id }</p>
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+        )
+    }
+}
+
+
+export default MovieList;
