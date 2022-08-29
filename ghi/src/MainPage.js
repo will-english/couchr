69a73897c448a707f657e2add7b2e2fdaa92cf8e
Christina Sherbahn <christiesherbahn@gmail.com>
2022-08-29 12:09:01 -0500

    useEffect get popularmovies

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -4,6 +4,26 @@ import * as React from "react";
 import "./index.css";
 
 function MainPage() {
+  const [popular, setPopular] = React.useState('');
+  const [genres, setGenres] = React.useState('');
+  const [picks, setPicks] = React.useState('');
+  const[error, setError] = React.useState('');
+
+  React.useEffect(()=> {
+    async function getPopular() {
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`
+      const response = await fetch(url);
+      if (response.ok){
+        const data = await response.json();
+        setPopular(data["results"]);
+      }
+      else{
+        setError("There was a problem with the response")
+      }
+    }
+    getPopular();
+  }, [setPopular, setError],
+  console.log(popular));
   return (
     <div className="px-4 mt-5 text-center">
       <h1>Couchr!</h1>
