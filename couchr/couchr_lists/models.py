e2cd5feff14f6e27a08aa606e7bb3b7e06611eaf
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-08-31 17:30:20 -0500

    backend views changed to accomodate auth, also journals

--- couchr/couchr_lists/models.py
+++ couchr/couchr_lists/models.py
@@ -8,17 +8,11 @@ class MovieVO(models.Model):
     api_url = models.URLField(unique=True, null=True, blank=True)
     api_id = models.CharField(max_length=100, unique=False, null=True, blank=True)
 
-# We will use the user model once auth all figured out
-
-# class User(models.Model):
-#     user_id = models.CharField(max_length=100, unique=False, null=True, blank=True)
-
 class List(models.Model):
     name = models.CharField(max_length=100, unique=False, null=True, blank=True)
     description = models.CharField(max_length=100, unique=False, null=True, blank=True)
     movies = models.ManyToManyField(
         MovieVO,
-        null=True,
         blank=True,
     )
     user = models.ForeignKey(
