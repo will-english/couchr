1f2400639e0fbb2ffc06ad2b66e98cfe1ea71f55
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-09-01 18:04:21 -0500

    created popup modal for new review form

--- ghi/src/Reviews/CreateNewReviewForm.js
+++ ghi/src/Reviews/CreateNewReviewForm.js
@@ -31,22 +31,44 @@ function NewReviewForm() {
         
         if (response.ok) {
             console.log("response ok")
+            const cleared = {
+                name: '',
+                description: '',
+                movie_id: 1,
+            };
+            setInput(cleared);
         }
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">name</label>
-                    <input onChange={handleInput} value={input.name} type="text" className="form-control" name="name" placeholder="name"/>
+            <button type="button" className="btn dropdown_list_button" data-bs-toggle="modal" data-bs-target="#reviewForm">
+                + New review
+            </button>
+            <div className="modal fade create_list_form" id="reviewForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">New List</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="col-form-label">name</label>
+                                    <input onChange={handleInput} value={input.name} type="text" className="form-control" name="name" placeholder="name"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description">description</label>
+                                    <input onChange={handleInput} value={input.description} type="text" className="form-control" name="description" placeholder="description"/>
+                                </div>
+                                <button type="button" className="btn btn-danger create_list_form_close_button" data-bs-dismiss="modal">Close</button>
+                                <button className="btn btn-primary" data-bs-dismiss="modal">Create</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="description">description</label>
-                    <input onChange={handleInput} value={input.description} type="text" className="form-control" name="description" placeholder="description"/>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </>
     )
 }
