aa6ece1235e7aafeb074d0084d5bc004ecda1d2a
Will English <willenglish55@gmail.com>
2022-09-01 14:55:06 -0500

    changes to user page, nav, app, and index.css

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -9,27 +9,11 @@ import Login from './auth/login';
 import Signup from './auth/signup';
 import { AuthProvider } from './auth/auth_provider';
 import MyMovieLists from './Watchlists/MyMovieLists';
+import UserDetail from './auth/UserDetail';
 
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
     <AuthProvider>
       <Nav />
       <div className="container">
@@ -40,6 +24,7 @@ function App() {
           <Route path='login/' element={<Login />} />
           <Route path='signup/' element={<Signup />} />
           <Route path='mylists/' element={<MyMovieLists />} />
+          <Route path='user_detail/' element={<UserDetail /> }/>
         </Routes>
       </div>
     </AuthProvider>
