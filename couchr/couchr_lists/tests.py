e2d1c1dd409b6667375b348d38ddcb58f57e5e59
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-09-12 17:18:41 -0500

    unit tests for backend

--- couchr/couchr_lists/tests.py
+++ couchr/couchr_lists/tests.py
@@ -1,16 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-# testing instructions
-
-# cd into couchr_lists
-# run command <<python -m pytest tests.py>>
-
-
-def random():
-    return "something"
-
-def test_random():
-    assert random() == "something"
