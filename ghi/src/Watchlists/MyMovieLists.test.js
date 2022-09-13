9f81c59bf98e484446e57faa982fb4027914247c
Jamie <jamieschuetter@gmail.com>
2022-09-13 15:08:31 -0500

    renamed and moved mymovielist test file

--- ghi/src/Watchlists/MyMovieLists.test.js
+++ ghi/src/Watchlists/MyMovieLists.test.js
@@ -1,11 +1,9 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect'
-import ControlledCarousel2 from './CouchrCarousel'
-// import MainPage from './MainPage'
-import MyMovieLists from '../Watchlists/MyMovieLists'
+import MyMovieLists from './MyMovieLists'
 
 
-it('has a radio button with proper settings', () => {
+it('render movie list name for the lists', () => {
   const { container } = render(<MyMovieLists
                                  />);
   const inputs = container.getElementsByTagName('h1');
