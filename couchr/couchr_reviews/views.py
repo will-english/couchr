4c05826531716a550a040b7942bf6ad39aae34f1
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-09-13 16:35:37 -0500

    reviews and journals

--- couchr/couchr_reviews/views.py
+++ couchr/couchr_reviews/views.py
@@ -12,7 +12,7 @@ from couchr_lists.models import MovieVO
 
 
 # get all reviews from a user
-# @auth.jwt_login_required
+@auth.jwt_login_required
 @require_http_methods(["GET", "POST"])
 def api_reviews(request, username):
     user = User.objects.get(username=username)
@@ -60,7 +60,7 @@ def api_reviews(request, username):
 
 
 # get a specific reviews from a user
-# @auth.jwt_login_required
+@auth.jwt_login_required
 @require_http_methods(["DELETE", "GET", "PUT"])
 def api_review(request, pk, username):
     user = User.objects.get(username=username)
