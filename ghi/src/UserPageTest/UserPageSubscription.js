1cbc26b09bf715c99e494ad07609d65e2120affe
Xiaoteng Xia <xiaxiaott@gmail.com>
2022-09-14 14:30:24 -0500

    change sign in CSS and subscrib CSS

--- ghi/src/UserPageTest/UserPageSubscription.js
+++ ghi/src/UserPageTest/UserPageSubscription.js
@@ -19,18 +19,93 @@ function UserPageSubscription() {
         <div className="userpage_left_content_area">
 
             <div className="userpage_left_content_area_content">
-                <h1>Subscription Page</h1>
+                <h1>Your Plan, Your Couchr</h1>
 
                 <div className="subscription_card">
+                    <h5 className="subscription_title">Basic</h5>
+                    <h1 className="price_text"><b>$29</b></h1>
+                    <p className="price_text">/month</p>
+                    <p className="subscription_discription"><b>The perfect way to get started and get used to our tools</b></p>
 
+                    <div className="subscription_page_block_line"></div>
+
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>All features in Standard</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <button className="btn btn-primary subscrib_button"><b>Get Started</b></button>
                 </div>
 
                 <div className="subscription_card">
+                    <h5 className="subscription_title">Standard</h5>
+                    <h1 className="price_text"><b>$49</b></h1>
+                    <p className="price_text">/month</p>
+                    <p className="subscription_discription"><b>The perfect way to get started and get used to our tools</b></p>
 
+                    <div className="subscription_page_block_line"></div>
+
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>All features in Standard</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <button className="btn btn-primary subscrib_button"><b>Get Started</b></button>
                 </div>
 
                 <div className="subscription_card">
+                    <h5 className="subscription_title">Premium</h5>
+                    <h1 className="price_text"><b>$99</b></h1>
+                    <p className="price_text">/month</p>
+                    <p className="subscription_discription"><b>The perfect way to get started and get used to our tools</b></p>
 
+                    <div className="subscription_page_block_line"></div>
+
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>All features in Standard</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <p className="subscription_feature_discription">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                        <b>balabalabala</b>
+                    </p>
+                    <button className="btn btn-primary subscrib_button"><b>Get Started</b></button>
                 </div>
 
             </div>
