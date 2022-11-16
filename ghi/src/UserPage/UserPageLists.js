692967f493fad64664f232a58f88f146cc4193be
Will English <willenglish55@gmail.com>
2022-11-16 13:49:22 -0600

    removing app.test.js file

--- ghi/src/UserPage/UserPageLists.js
+++ ghi/src/UserPage/UserPageLists.js
@@ -163,7 +163,7 @@ function UserPageLists() {
                             </div>
                         )
                     })}
-                    <div data-bs-toggle="modal" data-bs-target="#listForm" className="movie_user_list_card movie_create_user_list_card">
+                    <div data-bs-toggle="modal" data-bs-target="#listForm" className="new_user_list_card movie_create_user_list_card">
                         <div className="movie_create_user_list_card_icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
